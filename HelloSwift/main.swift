19971676b6b2abf040d7b630821848740fea3394
ethanq84 <qiaofeng@qiaofengdeMacBook-Pro.local>
2017-03-04 23:17:48 +0800

    Initial Commit

---
+++ HelloSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HelloSwift
+//
+//  Created by qiaofeng on 2017/3/4.
+//  Copyright © 2017年 qiaofeng. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
